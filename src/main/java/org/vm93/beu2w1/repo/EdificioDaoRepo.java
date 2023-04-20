4cfe0c42f530f8e1c62e22277b63454a987113b3
Vincenzo M <vincenzomaiorana@gmail.com>
2023-04-20 10:04:03 +0200

    Update EdificioDaoRepo.java

--- src/main/java/org/vm93/beu2w1/repo/EdificioDaoRepo.java
+++ src/main/java/org/vm93/beu2w1/repo/EdificioDaoRepo.java
@@ -3,12 +3,13 @@ package org.vm93.beu2w1.repo;
 
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 import org.vm93.beu2w1.model.Edificio;
 
 
 @Repository
-public interface EdificioDaoRepo extends CrudRepository<Edificio, Long> {
+public interface EdificioDaoRepo extends PagingAndSortingRepository<Edificio, Long>, CrudRepository<Edificio, Long> {
 	
 	@Query(value = "SELECT e FROM Edificio e ORDER BY RANDOM() LIMIT 1")
 	public Edificio edificioRandom();
